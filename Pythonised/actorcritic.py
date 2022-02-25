15421839ed40a30d1a30a58b1450318bd36a03dc
Martyn <martyn.smith560@outlook.com>
2022-02-25 11:22:00 +0000

    reformatting

--- Pythonised/actorcritic.py
+++ Pythonised/actorcritic.py
@@ -2,21 +2,28 @@ import os
 import tensorflow.keras as keras
 from tensorflow.keras.layers import Dense
 
+
 class ActorCriticNetwork(keras.Model):
-    def __init__(self, n_actions, fc1_dims=1024, fc2_dims=512,
-            name='actor_critic', chkpt_dir='tmp/actor_critic'):
+    def __init__(
+        self,
+        n_actions,
+        fc1_dims=1024,
+        fc2_dims=512,
+        name="actor_critic",
+        chkpt_dir="tmp/actor_critic",
+    ):
         super(ActorCriticNetwork, self).__init__()
         self.fc1_dims = fc1_dims
         self.fc2_dims = fc2_dims
         self.n_actions = n_actions
         self.model_name = name
         self.checkpoint_dir = chkpt_dir
-        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_ac')
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name + "_ac")
 
-        self.fc1 = Dense(self.fc1_dims, activation='relu')
-        self.fc2 = Dense(self.fc2_dims, activation='relu')
+        self.fc1 = Dense(self.fc1_dims, activation="relu")
+        self.fc2 = Dense(self.fc2_dims, activation="relu")
         self.v = Dense(1, activation=None)
-        self.pi = Dense(n_actions, activation='softmax')
+        self.pi = Dense(n_actions, activation="softmax")
 
     def call(self, state):
         value = self.fc1(state)
--- Pythonised/red.py
+++ Pythonised/red.py
@@ -1,16 +1,18 @@
 from agent import Agent
 import logging
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  #FATAL only
-logging.getLogger('tensorflow').setLevel(logging.FATAL)
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # FATAL only
+logging.getLogger("tensorflow").setLevel(logging.FATAL)
 import tensorflow as tf
-#import tensorflow_probability as tfp
+
+# import tensorflow_probability as tfp
 from actorcritic import ActorCriticNetwork
 
 GAMMA = 0.95
 
-class ThreatAgent(Agent):
 
+class ThreatAgent(Agent):
     def __init__(self):
         self.id = "red"
         self.gamma = GAMMA
@@ -20,22 +22,23 @@ class ThreatAgent(Agent):
     def learn(self, previous, reward, observation, done):
         previous = tf.convert_to_tensor([previous], dtype=tf.float32)
         observation = tf.convert_to_tensor([observation], dtype=tf.float32)
-        reward = tf.convert_to_tensor(reward, dtype=tf.float32) # not fed to NN
+        reward = tf.convert_to_tensor(reward, dtype=tf.float32)  # not fed to NN
         with tf.GradientTape(persistent=True) as tape:
             prev_value, actions = self.actor_critic(previous)
             obs_value, _ = self.actor_critic(observation)
             prev_value = tf.squeeze(prev_value)
             obs_value = tf.squeeze(obs_value)
 
-            delta = reward + self.gamma*obs_value*(1-int(done)) - prev_value
-            #ecce here, wtf does log mean for a vector value?
-            actor_loss = -actions*delta
+            delta = reward + self.gamma * obs_value * (1 - int(done)) - prev_value
+            # ecce here, wtf does log mean for a vector value?
+            actor_loss = -actions * delta
             critic_loss = delta**2
             total_loss = actor_loss + critic_loss
 
         gradient = tape.gradient(total_loss, self.actor_critic.trainable_variables)
-        self.actor_critic.optimizer.apply_gradients(zip(
-            gradient, self.actor_critic.trainable_variables))
+        self.actor_critic.optimizer.apply_gradients(
+            zip(gradient, self.actor_critic.trainable_variables)
+        )
 
     def get_action(self, observation):
         observation = tf.convert_to_tensor([observation])
