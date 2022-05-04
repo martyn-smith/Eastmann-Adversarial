382f7ca3e457593936e1dc70bab98425ca1b7a05
Martyn <martyn.smith560@outlook.com>
2022-05-04 13:32:36 +0100

    refactoring

--- Pythonised/blue.py
+++ Pythonised/blue.py
@@ -1,38 +1,25 @@
-from agent import Agent
-import logging
-import os
 import numpy as np
-
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # FATAL only
-logging.getLogger("tensorflow").setLevel(logging.FATAL)
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-
-# import tensorflow_probability as tfp
-from actorcritic import ActorCriticNetwork
-
-GAMMA = 0.95
-
+from agent import Agent
+from subprocess import run
 
 class DefendAgent(Agent):
     def __init__(self):
         self.id = "blue"
-        self.gamma = GAMMA
-        self.actor_critic = ActorCriticNetwork(n_actions=1)
-        self.actor_critic.compile(optimizer="adam")
         self.stategenerator = load_model("./Pythonised/stategenerator")
+        super().__init(1)
 
     def __call__(self, observation):
-        state = self.stategenerator.predict([observation])[0]
-        with open("teststates/state.in") as f:
-            state = f.read()
-        xmeas = np.frombuffer(run(["../te", "-l", "-v"], input=bytes(state, "utf-8"), capture_output=True).stdout)
-        print(xmeas)
-        value = sum((self.gamma ** i) * 20_000 * xmeas[17] for i, xmeas in enumerate(xmeas))
-        # print(f"{state=}, {value=}")
-        v = value
-        pi = self.pi(value)
+         print(f"{state=}, {value=}")
+        v = self.value()
+        pi = self.pi()
         return v, pi
+
+    def value(self, observation):
+        state = self.stategenerator.predict([observation])[0]
+        xmeas = np.frombuffer(run(["../te", "-l", "-v"], input=bytes(state, "utf-8"), capture_output=True).stdout)
+        #print(xmeas)
+        value = sum((self.gamma ** i) * 20_000 * xmeas[17] for i, xmeas in enumerate(xmeas))
+        return value
 
     def learn(self, previous, reward, observation, done):
         previous = tf.convert_to_tensor([previous], dtype=tf.float32)
@@ -40,7 +27,7 @@ class DefendAgent(Agent):
         reward = tf.convert_to_tensor(reward, dtype=tf.float32)  # not fed to NN
         with tf.GradientTape(persistent=True) as tape:
             prev_value, actions = self.actor_critic(previous)
-            obs_value, _ = self.actor_critic(observation)
+            obs_value, _ = self.value(observation)
             prev_value = tf.squeeze(prev_value)
             obs_value = tf.squeeze(obs_value)
 
@@ -55,8 +42,3 @@ class DefendAgent(Agent):
             zip(gradient, self.actor_critic.trainable_variables)
         )
         return total_loss.numpy()[0][0]
-
-    def get_action(self, observation):
-        observation = tf.convert_to_tensor([observation])
-        value, actions = self(observation)
-        return actions.numpy()[0]
