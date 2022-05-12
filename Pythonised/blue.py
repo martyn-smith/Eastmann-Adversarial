0b30384b2b5ea2e1051ca5caea4d90b791172e85
Martyn <martyn.smith560@outlook.com>
2022-05-12 13:55:01 +0100

    tensorised value from twin

--- Pythonised/blue.py
+++ Pythonised/blue.py
@@ -40,25 +40,27 @@ class DefendAgent(Agent):
     def learn(self, previous, reward, observation, done):
         value = self.value(observation)
         value = tf.convert_to_tensor([value], dtype=tf.float32)
-        return super().learn(previous, reward, observation, done)
-        # previous = tf.convert_to_tensor([previous], dtype=tf.float32)
-        # observation = observation[:42]
-        # observation = tf.convert_to_tensor([observation], dtype=tf.float32)
-        # reward = tf.convert_to_tensor(reward, dtype=tf.float32)  # not fed to NN
-        # with tf.GradientTape(persistent=True) as tape:
-        #     prev_value, actions = self.actor_critic(previous)
-        #     obs_value, _ = self.actor_critic(observation)
-        #     prev_value = tf.squeeze(prev_value)
-        #     obs_value = tf.squeeze(obs_value)
-        #
-        #     delta = reward + self.gamma * obs_value * (1 - int(done)) - prev_value
-        #     # ecce here
-        #     actor_loss = -actions * delta
-        #     critic_loss = delta**2
-        #     total_loss = actor_loss + critic_loss
-        #
-        # gradient = tape.gradient(total_loss, self.actor_critic.trainable_variables)
-        # self.actor_critic.optimizer.apply_gradients(
-        #     zip(gradient, self.actor_critic.trainable_variables)
-        # )
-        # return total_loss.numpy()[0][0]
+        # return super().learn(previous, reward, observation, done)
+        print(value)
+        previous = tf.convert_to_tensor([previous], dtype=tf.float32)
+        observation = observation[:42]
+        observation = tf.convert_to_tensor([observation], dtype=tf.float32)
+        reward = tf.convert_to_tensor(reward, dtype=tf.float32)  # not fed to NN
+        with tf.GradientTape(persistent=True) as tape:
+            prev_value, actions = self.actor_critic(previous)
+            # obs_value, _ = self.actor_critic(observation)
+            obs_value = value
+            prev_value = tf.squeeze(prev_value)
+            obs_value = tf.squeeze(obs_value)
+
+            delta = reward + self.gamma * obs_value * (1 - int(done)) - prev_value
+            # ecce here
+            actor_loss = -actions * delta
+            critic_loss = delta**2
+            total_loss = actor_loss + critic_loss
+
+        gradient = tape.gradient(total_loss, self.actor_critic.trainable_variables)
+        self.actor_critic.optimizer.apply_gradients(
+            zip(gradient, self.actor_critic.trainable_variables)
+        )
+        return total_loss.numpy()[0][0]
