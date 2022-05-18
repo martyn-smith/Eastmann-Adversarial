4171d483d486937aba64bb5169720e1b785729ea
Martyn <martyn.smith560@outlook.com>
2022-05-18 16:26:34 +0100

    added scenarios to makefile; red team intent bugfix

--- Pythonised/teprob.py
+++ Pythonised/teprob.py
@@ -1124,7 +1124,7 @@ class TEproc(gym.Env):
         elif self.red_intent == "recipe":
             red_reward = -(self.production(red_xmeas) - self.utilities(red_xmeas))
         elif self.red_intent == "destruction":
-            self.red_intent = -self.mechanical(red_xmeas)
+            red_reward = -self.mechanical(red_xmeas)
         self.time += DELTA_t
         self.time_since_gas += DELTA_t
         return (
@@ -1421,14 +1421,13 @@ parser.add_argument(
     type=int,
     default=100,
 )
-parser.add_argument(
-    "--peaceful",
-    help="no red or blue team action (overrides scenario)",
-    action="store_true",
-)
+group = parser.add_mutually_exclusive_group()
+group.add_argument("--nored", help="no red team actions", action="store_true")
+group.add_argument("--noblue", help="no blue team actions", action="store_true")
+group.add_argument("--peaceful", help="no blue or red team actions", action="store_true")
+
 parser.add_argument("--render", help="live visualisations (slow)", action="store_true")
 parser.add_argument("--report", help="generates report template", action="store_true")
-parser.add_argument("--scenario", help="select from scenarios:", default="default")
 parser.add_argument(
     "-v", "--verbose", help="displays debug info", action="count", default=0
 )
