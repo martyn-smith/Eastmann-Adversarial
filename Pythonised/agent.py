9e673652c5224e8964c0384ac9568200e2d3d907
Martyn <martyn.smith560@outlook.com>
2022-08-01 11:39:02 +0100

    added scaling to agent output

--- Pythonised/agent.py
+++ Pythonised/agent.py
@@ -38,16 +38,18 @@ class Agent:
         self.memory = deque(maxlen=100_000)
         self.batch_size = 64
         self.gamma = 0.95
-        self.epsilon = 1.0
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.095
+        self.epsilon = 0.0001
+        self.rng = np.random.random
+        self.scale = 100.0
         self.actor_critic = ActorCriticNetwork(n_out)
         self.actor_critic.compile(optimizer="adam")
 
     def __call__(self, observation):
         observation = tf.convert_to_tensor([observation])
         value, actions = self.actor_critic(observation)
-        return actions.numpy()[0]
+        return np.clip(actions.numpy()[0] + (self.epsilon * self.rng()) * self.scale,
+                       0.,
+                       100.)
 
     def learn(self, previous, reward, observation, done):
         previous = tf.convert_to_tensor([previous], dtype=tf.float32)
